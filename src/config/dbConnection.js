f6caf7ef1959093dd2dc8ba88ff2b7e76d0f7a13
JOstin-Orbe <63017264+Jostiin@users.noreply.github.com>
2023-01-31 19:56:46 -0500

    Add files via upload

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,11 +1,23 @@
+
 const mysql = require('mysql');
 
 module.exports = () => {
     return mysql.createConnection({
-        host: 'brj0ndeqg40musybuxmz-mysql.services.clever-cloud.com',
-        user: 'ulmzpjr949kkvc8m',
-        database: 'brj0ndeqg40musybuxmz',
+        host:'brj0ndeqg40musybuxmz-mysql.services.clever-cloud.com',
+        user:'ulmzpjr949kkvc8m',
+        database:'brj0ndeqg40musybuxmz',
         password:'9n50XZoHo1XESm03yKDJ',
         port:'3306'
     });
 };
+/*
+module.exports = () => {
+    return mysql.createConnection({
+        host:'containers-us-west-181.railway.app',
+        user:'root',
+        database:'railway',
+        password:'wze5bM5Mo6YZfInGBuz6',
+        port:'7375'
+    });
+};
+*/
